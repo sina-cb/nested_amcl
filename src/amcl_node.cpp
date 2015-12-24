0e690c6cb5ef3efca7143a2d8bd8d69fad6459d6
Sina Solaimanpour <sina@uga.edu>
2015-12-23 22:19:55 -0500

    It is finally working

--- src/amcl_node.cpp
+++ src/amcl_node.cpp
@@ -615,9 +615,7 @@ AmclNode::AmclNode() :
         color_angles[i] = false;
     }
 
-
-
-
+    ROS_ERROR("End of constructor");
 
 }
 
@@ -869,6 +867,7 @@ AmclNode::handleMapMessage(const nav_msgs::OccupancyGrid& msg)
     // try to apply the initial pose now that the map has arrived.
     applyInitialPose();
 
+    ROS_ERROR("End of Map handleMapMessage");
 }
 
 void
@@ -1724,11 +1723,9 @@ AmclNode::laserReceived(const sensor_msgs::LaserScanConstPtr& laser_scan)
 
     }
 
-
     // Calculate the pose to be published as the current pose estimate by AMCL
-
     if(resampled || force_publication)
-    {
+    {     
         // Read out the current hypotheses
         double max_weight = 0.0;
         int max_weight_hyp = -1;
@@ -1863,10 +1860,9 @@ AmclNode::laserReceived(const sensor_msgs::LaserScanConstPtr& laser_scan)
 
             // subtracting base to odom from map to base and send map to odom instead
             tf::Stamped<tf::Pose> odom_to_map;
-            tf::Stamped<tf::Pose> odom_to_map_true;
+            // tf::Stamped<tf::Pose> odom_to_map_true;
             try
             {
-
                 /* !!!!!
                    This is the actual ODOM Pose that needs to be published by AMCL.
                    Turning this off for the moment to get videos.
@@ -1878,10 +1874,10 @@ AmclNode::laserReceived(const sensor_msgs::LaserScanConstPtr& laser_scan)
                                                  hyps[max_weight_hyp].pf_pose_mean.v[1],
                                                  0.0));
 
-                tf::Transform tmp_tf_true(tf::createQuaternionFromYaw(hyps[max_weight_hyp].pf_pose_mean.v[2]-0.02),
-                                     tf::Vector3(true_pose_normal.v[0]-0.8,
-                                                 true_pose_normal.v[1]-0.0,
-                                                 0.0));
+                // tf::Transform tmp_tf_true(tf::createQuaternionFromYaw(hyps[max_weight_hyp].pf_pose_mean.v[2]-0.02),
+                //                      tf::Vector3(true_pose_normal.v[0]-0.8,
+                //                                  true_pose_normal.v[1]-0.0,
+                //                                  0.0));
 
 
 
@@ -1889,23 +1885,23 @@ AmclNode::laserReceived(const sensor_msgs::LaserScanConstPtr& laser_scan)
                                                       laser_scan->header.stamp,
                                                       base_frame_id_);
 
-                tf::Stamped<tf::Pose> tmp_tf_stamped_true (tmp_tf_true.inverse(),
-                                                      laser_scan->header.stamp,
-                                                           "/robot1_tf_true/base_link");
-
+                // tf::Stamped<tf::Pose> tmp_tf_stamped_true (tmp_tf_true.inverse(),
+                //                                       laser_scan->header.stamp,
+                //                                            "/robot1_tf_true/base_link");
 
 
                 this->tf_->transformPose(odom_frame_id_,
                                          tmp_tf_stamped,
                                          odom_to_map);
 
-                this->tf_->transformPose("/robot1_tf_true/odom",
-                                         tmp_tf_stamped_true,
-                                         odom_to_map_true);
+                // this->tf_->transformPose("/robot1_tf_true/odom",
+                //                          tmp_tf_stamped_true,
+                //                          odom_to_map_true);
+
             }
             catch(tf::TransformException)
             {
-                ROS_DEBUG("Failed to subtract base to odom transform");
+                ROS_ERROR("Failed to subtract base to odom transform");
                 return;
             }
 
@@ -1913,8 +1909,8 @@ AmclNode::laserReceived(const sensor_msgs::LaserScanConstPtr& laser_scan)
             latest_tf_ = tf::Transform(tf::Quaternion(odom_to_map.getRotation()),
                                        tf::Point(odom_to_map.getOrigin()));
 
-            latest_tf_true = tf::Transform(tf::Quaternion(odom_to_map_true.getRotation()),
-                                       tf::Point(odom_to_map_true.getOrigin()));
+            // latest_tf_true = tf::Transform(tf::Quaternion(odom_to_map_true.getRotation()),
+            //                            tf::Point(odom_to_map_true.getOrigin()));
 
 
 
@@ -1929,15 +1925,15 @@ AmclNode::laserReceived(const sensor_msgs::LaserScanConstPtr& laser_scan)
                                                 transform_expiration,
                                                 global_frame_id_, odom_frame_id_);
 
-            tf::StampedTransform tmp_tf_stamped_true(latest_tf_true.inverse(),
-                                                transform_expiration,
-                                                     "/map", "/robot1_tf_true/odom");
+            // tf::StampedTransform tmp_tf_stamped_true(latest_tf_true.inverse(),
+            //                                     transform_expiration,
+            //                                          "/map", "/robot1_tf_true/odom");
 
 
 
 
             this->tfb_->sendTransform(tmp_tf_stamped);
-            this->tfb_->sendTransform(tmp_tf_stamped_true);
+            // this->tfb_->sendTransform(tmp_tf_stamped_true);
 
             sent_first_transform_ = true;
 
