8b35acfe0d69328a6a2dbe20d40d995af675e621
Sina Solaimanpour <sina@uga.edu>
2016-02-29 19:50:35 -0500

    Added Hector Trajectory Server Support

--- src/amcl_node.cpp
+++ src/amcl_node.cpp
@@ -531,7 +531,7 @@ AmclNode::AmclNode() :
     std::string home_path = std::string(getenv("HOME"));
 
 
-    algo_name = "LMCHMM";
+    algo_name = "STATIC_MOTION";
     std::string file_name;
 
     private_nh_.param("robot_start_config_id", robot_start_config_id, std::string("DefaultConfig") );
