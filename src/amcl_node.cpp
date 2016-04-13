3a3c0c3ff8c9196f19a39767fc6700fc908eefc6
Sina Solaimanpour <sina@uga.edu>
2016-04-13 14:28:33 -0400

    Ready for experiments

--- src/amcl_node.cpp
+++ src/amcl_node.cpp
@@ -531,7 +531,7 @@ AmclNode::AmclNode() :
     std::string home_path = std::string(getenv("HOME"));
 
 
-    algo_name = "STATIC_MOTION";
+    algo_name = "RANDOM_MOTION";
     std::string file_name;
 
     private_nh_.param("robot_start_config_id", robot_start_config_id, std::string("DefaultConfig") );
